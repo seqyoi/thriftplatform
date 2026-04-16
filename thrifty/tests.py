04a0d8981ea502b6a3b5a5f97ed20ae6fea4f115
seqyoi <selusdud@gmail.com>
2026-04-16 19:13:27 +0545

    Few Fixes

--- thrifty/tests.py
+++ thrifty/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from thrifty.models import Product, Profile
+from django.contrib.auth.models import User
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name="Test Product", description="This is a test product.", price=9.99)
+    def test_product_creation(self):
+        self.assertEqual(self.product.name, "Test Product")
+        self.assertEqual(self.product.description, "This is a test product.")
+        self.assertEqual(self.product.price, 9.99)
+
+class UserAuthenticationTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+    def test_user_authentication(self):
+        login_successful = self.client.login(username='testuser', password='testpassword')
+        self.assertTrue(login_successful)
+
+from django.urls import reverse 
